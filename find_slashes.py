358f94aa650e65e0eeb8a3b34793696f8b7ecf01
serepasf <serepasf@gmail.com>
2017-02-14 12:20:24 +0200

    Add another feature. When between // there is * create a list For example 'y01*y04',create [y01,y02,y03,y04]

--- find_slashes.py
+++ find_slashes.py
@@ -1,5 +1,6 @@
 #re.findall('/.*?/', set[2])[0].replace('/','').strip()
 import re
+from init_list import init_list as il
 
 def find_slashes(text):
 	text = re.findall('/.*?/', text)
@@ -8,4 +9,6 @@ def find_slashes(text):
 		text = text[0].replace('/','').strip()
 		if text.find(',') != -1:
 			text = text.split(',')
+		if text.find('*') != -1:
+			text = il(text)
 		return text
