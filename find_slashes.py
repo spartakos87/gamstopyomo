1fa75464db72c84de57b77dd910f948feed2931c
serepasf <serepasf@gmail.com>
2017-02-14 11:40:07 +0200

    Fix bug

--- find_slashes.py
+++ find_slashes.py
@@ -2,11 +2,10 @@
 import re
 
 def find_slashes(text):
-	text = re.findall('/.*?/', set[2])[0].replace('/','').strip()
+	text = re.findall('/.*?/', text)
+	#[0].replace('/','').strip()
 	if len(text) > 0:
-		if len(text) == 1:
-			text = text[0]
-		else:
-			if text.find(',') != -1:
-				text = text.split(',')
-	return text
+		text = text[0].replace('/','').strip()
+		if text.find(',') != -1:
+			text = text.split(',')
+		return text
